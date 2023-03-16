36885ca966ef346ecdfbb81e97f3ee07afb96d9b
bontlenkoale1 <bontlenkoale20@gmail.com>
2023-03-16 13:58:35 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry
- * return: Always 0
+ * 
+ * return: 0 Always 
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
