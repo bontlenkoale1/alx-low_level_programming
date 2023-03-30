40ce02f2157337de101b255a752898a2d2cd70aa
bontlenkoale1 <bontlenkoale20@gmail.com>
2023-03-30 16:44:35 -0700

    0strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -19,9 +19,9 @@ char *_strcat(char *dest, char *src)
 	}
 	for (i = 0; src[i] != 0; i++)
 	{
-		dest[dlen] = drc[i];
-		dlen++
+		dest[dlen] = src[i];
+		dlen++;
 	}
-	dest[dlen] = '\0';
+	dest[dlen] = ('\0');
 	return (dest);
 }
