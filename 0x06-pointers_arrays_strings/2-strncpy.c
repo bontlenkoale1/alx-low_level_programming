57dd507f540816be728234117a4bb9ec44c05244
bontlenkoale1 <bontlenkoale20@gmail.com>
2023-03-31 16:57:51 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,47 +1,29 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- * Return: 0
+ * _strncpy - copy a string with n
+ * @dest: copy to
+ * @src: copy from
+ * @n: number of char to be copied
+ * Return: dest
  */
 
-int main(void)
+char *_strncpy(char *dest, char *src, int n)
 
 {
+	int j;
 
-char s1[98];
-char *ptr;
-int i;
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
 
-for (i = 0; i < 98 - 1; i++)
-{
-s1[1] = ('*');
+	return (dest);
 }
-s1[i] = '\0';
-printf("%s\n", s1);
-ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-printf("%s\n", s1);
-printf("%s\n", ptr);
-ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 98);
-printf("%s", s1);
-printf("%s", ptr);
-
-for (i = 0; i < 98; i++)
-
-{
-if (i % 10)
-{
-printf(" ");
-
-}
-if (!(i % 10) && i)
-{
-printf("\n");
-}
-printf("0x%02x", s1[i]);
-}
-printf("\n");
-return (0);
-}
-
