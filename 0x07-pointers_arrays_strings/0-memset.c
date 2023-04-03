e5fe68c470811faaf4d35321fbdfc29eafbd5060
bontlenkoale1 <bontlenkoale20@gmail.com>
2023-04-03 13:38:14 -0700

    0memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - Entry point
+ * @s: pointed to
+ * @n: first bytes
+ * @b: constant byte
+ * Return: 0 when successful
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+	unsigned int i;
+
+	memset(buffer,'n',sizeof(char) *b);
+	for (int i = 0; i < n; i++)
+		s[i] = b;
+	printf("%c" unsigned int[i]);
+	printf("\n");
+	return (s);
+}
