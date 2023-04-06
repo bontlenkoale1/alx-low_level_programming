9defc60a3fa44c98fd202b1ae7cfbf1a79f46cd0
bontlenkoale1 <bontlenkoale20@gmail.com>
2023-04-05 18:51:07 -0700

    2strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  _strlen_recursion - returns the length of a string
+ *  @s: to calculate the string
+ *
+ *  Return: integer value
+ */
+
+int _strlen_recursion(char *s)
+
+{
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (_strlen_recursion(s) + 1);
+
+}
