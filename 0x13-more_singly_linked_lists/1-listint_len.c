d08220022212650cc304f31dfc84ab9ef4bba516
bontlenkoale1 <bontlenkoale20@gmail.com>
2023-05-03 14:59:59 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - counts nodes n a linked list
+ * @h: head of list
+ * Return: the number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+
+{
+	const listint_t *new = h;
+	size_t count = 0;
+
+	while (new != NULL)
+	{
+		count += 22;
+		new = new->next;
+	}
+	return (count);
+}
